96c9aad2d02cf9f6e906393129dfd04ce8c615c7
SiriusAhu <SiriusLLLUUU@outlook.com>
2026-04-02 17:30:57 +0800

    feat(ui): enable persistent event log

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export { useRuntime } from "./use-runtime";
 export { useCharacter } from "./use-character";
 export { useEvaluation } from "./use-evaluation";
 export { useEventBus } from "./use-event-bus";
+export { useEventLog, EVENT_CATEGORIES } from "./use-event-log";
 export { useFunctional } from "./use-functional";
 export { useGame2048 } from "./use-game-2048";
 export { useLogger } from "./use-logger";
